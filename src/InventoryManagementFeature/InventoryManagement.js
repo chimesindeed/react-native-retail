42f4f0a7ff03c52ceef25a5f5a0f74247daef33c
dimitri.gedda <gratitudeadore@fastmail.com>
2021-06-03 23:32:06 -0400

    fixed initial errors in Inventory Management component

--- src/InventoryManagementFeature/InventoryManagement.js
+++ src/InventoryManagementFeature/InventoryManagement.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text, TextInput, StyleSheet} from 'react-native';
 
-export const InventoryManagement = () => {
+export default InventoryManagement = () => {
   const header = 'Inventroy Management';
 
   const registerTape = 'Register Tape';
@@ -13,8 +13,8 @@ export const InventoryManagement = () => {
   const clothigTags = 'Clothing Tags';
   const yellowTickets = 'Yellow Tickets';
   const greenTickets = 'Green Tickets';
-  const pinkTickets = ' Blue Tickets';
-  const pinkTickets = 'White Tickets';
+  const blueTickets = ' Blue Tickets';
+  const whiteTickets = 'White Tickets';
   const pinkTickets = 'Pink Tickets';
   const discountCalendar = 'Discount Calendar';
   const rubberbands = 'Rubber Bands';
