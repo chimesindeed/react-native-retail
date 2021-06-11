26e26694da6eb3bab4100d0f65e0bf7670a15a32
dimitri.gedda <gratitudeadore@fastmail.com>
2021-06-10 23:28:26 -0400

    added sample Notes array

--- src/NotesFeature/Notes.js
+++ src/NotesFeature/Notes.js
@@ -3,6 +3,21 @@ import {View, Text, TextInput, StyleSheet} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const Notes = () => {
+
+
+  sampleNotesArray = [
+  {title: "Note1Title", body: "Note1Body"},
+  {title: "Note2Title", body: "Note2Body"},
+  {title: "Note13Title", body: "Note3Body"},
+  {title: "Note4Title", body: "Note4Body"},
+  {title: "Note5Title", body: "Note5Body"}, ]
+
+  setNotesToState(notes) {
+    notes.map((note)=>{
+      
+    })
+
+  }
   // on mount will load items from Async Storage into items variable in State
   const [items, setItems] = useState('');
 
@@ -10,7 +25,8 @@ export const Notes = () => {
   const [item, setItem] = useState('');
 
   // on mount will load Notes from Async Storage to Display
-  useEffect();
+  useEffect(
+    setItemsToState(sampleNotesArray));
   return (
     <View>
       <Text>Inside Notes Component</Text>
