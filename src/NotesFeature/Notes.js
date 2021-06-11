075ab97d352311ba646533b7762e2665f7ba5f7c
dimitri.gedda <gratitudeadore@fastmail.com>
2021-06-10 23:34:31 -0400

    setNotesToState loads notes into state on mount

--- src/NotesFeature/Notes.js
+++ src/NotesFeature/Notes.js
@@ -3,30 +3,27 @@ import {View, Text, TextInput, StyleSheet} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const Notes = () => {
+  const sampleNotesArray = [
+    {title: 'Note1Title', body: 'Note1Body'},
+    {title: 'Note2Title', body: 'Note2Body'},
+    {title: 'Note13Title', body: 'Note3Body'},
+    {title: 'Note4Title', body: 'Note4Body'},
+    {title: 'Note5Title', body: 'Note5Body'},
+  ];
 
-
-  sampleNotesArray = [
-  {title: "Note1Title", body: "Note1Body"},
-  {title: "Note2Title", body: "Note2Body"},
-  {title: "Note13Title", body: "Note3Body"},
-  {title: "Note4Title", body: "Note4Body"},
-  {title: "Note5Title", body: "Note5Body"}, ]
-
-  setNotesToState(notes) {
-    notes.map((note)=>{
-      
-    })
-
-  }
+  const setNotesToState = allNotes => {
+    allNotes.map(note => {
+      setNotes([...notes, allNotes]);
+    });
+  };
   // on mount will load items from Async Storage into items variable in State
-  const [items, setItems] = useState('');
+  const [notes, setNotes] = useState([]);
 
-  // to store newly created Item
-  const [item, setItem] = useState('');
+  // to store nnewly created Item
+  const [note, setNote] = useState('');
 
   // on mount will load Notes from Async Storage to Display
-  useEffect(
-    setItemsToState(sampleNotesArray));
+  useEffect(setNotesToState(sampleNotesArray));
   return (
     <View>
       <Text>Inside Notes Component</Text>
